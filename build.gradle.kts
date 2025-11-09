804796ce64d54a1b8d18b5c500192f7370d8966f
Abdel Malik <137098823+malik-dev635@users.noreply.github.com>
2025-11-09 17:18:00 +0000

    Fix: Resolve Gradle plugin version conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.5.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
-    }
-}
-
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
